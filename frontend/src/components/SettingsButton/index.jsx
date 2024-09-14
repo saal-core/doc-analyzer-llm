078abb1a411c3faf12e0271950cbd9d479d02f9f
Divyaansh <divyaansh@saal.ai>
2024-09-14 08:35:45 +0530

    Add gear icon for menu

--- frontend/src/components/SettingsButton/index.jsx
+++ frontend/src/components/SettingsButton/index.jsx
@@ -1,6 +1,6 @@
 import useUser from "@/hooks/useUser";
 import paths from "@/utils/paths";
-import { ArrowUUpLeft, Wrench } from "@phosphor-icons/react";
+import { ArrowUUpLeft, Wrench, GearSix } from "@phosphor-icons/react";
 import { Link } from "react-router-dom";
 import { useMatch } from "react-router-dom";
 import { ToolTipWrapper } from "../Footer";
@@ -43,21 +43,23 @@ export default function SettingsButton() {
     <Link
       to={!!user?.role ? paths.settings.system() : paths.settings.appearance()}
       className="transition-all duration-300 p-1 text-black"
-      style={{ width: "95%", paddingLeft: "8px" }}
+      // style={{ width: "95%", paddingLeft: "8px" }}
+      style={{ paddingLeft: "8px" }}
       aria-label="Home"
       data-tooltip-id="go-home"
       data-tooltip-content="Back to workspaces"
     >
       <div
-        style={{ alignItems: "center", width: "100%" }}
-        className={`transition-all duration-[200ms] text-sm
-          flex flex-grow w-[75%] gap-x-2 py-[8px] px-[12px] rounded-[4px] text-black justify-start items-center
-          hover:bg-workspace-item-selected-gradient hover:font-bold border-2 border-outline
-          `}
+        // style={{ alignItems: "center", width: "100%" }}
+        style={{ alignItems: "center", position: "fixed", width: "59px", display: "flex", left: "-40px" }}
+        // className={`transition-all duration-[200ms] text-sm
+        //   flex flex-grow w-[75%] gap-x-2 py-[8px] px-[12px] rounded-[4px] text-black justify-start items-center
+        //   hover:bg-workspace-item-selected-gradient hover:font-bold border-2 border-outline
+        //   `}
       >
-        <Wrench className="h-5 w-5" weight="fill" />
+        <GearSix className="h-5 w-5" weight="regular" />
 
-        <span>Settings</span>
+        {/* <span>Settings</span> */}
       </div>
     </Link>
     // </ToolTipWrapper>
