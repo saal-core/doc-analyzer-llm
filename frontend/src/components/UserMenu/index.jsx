0b68a452c4418e33bd7ecd0901cc7d5dff4d557b
Divyaansh <divyaansh@saal.ai>
2024-09-16 11:12:44 +0530

    Add Folder icon

--- frontend/src/components/UserMenu/index.jsx
+++ frontend/src/components/UserMenu/index.jsx
@@ -1,4 +1,8 @@
 import UserButton from "./UserButton";
+import { Chats } from "@phosphor-icons/react";
+import { Link } from "react-router-dom";
+import paths from "@/utils/paths";
+
 
 export default function UserMenu({ children }) {
   return (
@@ -11,9 +15,27 @@ export default function UserMenu({ children }) {
             background: "#030852",
             top: "75px",
             bottom: 0,
-            left: 0
+            left: 0,
           }}
-        />
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "22px",
+              cursor: "pointer",
+              height: "40px",
+              borderLeft: "4px solid #00A5D4",
+              alignItems: "center",
+            }}
+          >
+            <Link
+              to={paths.home()}
+            >
+              <Chats color="#00A5D4" size={28} />
+            </Link>
+          </div>
+        </div>
       )}
       <UserButton />
       {children}
